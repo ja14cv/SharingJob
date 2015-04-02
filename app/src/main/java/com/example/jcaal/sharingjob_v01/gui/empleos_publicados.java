4b10a9dd33adf941faeb33ae8885e8ba51f5e48a
ja14cv <ja14cv@gmail.com>
2015-04-02 12:33:01 -0600

    - agregar clases faltantes.

---
+++ app/src/main/java/com/example/jcaal/sharingjob_v01/gui/empleos_publicados.java
@@ -0,0 +1,38 @@
+package com.example.jcaal.sharingjob_v01.gui;
+
+import com.example.jcaal.sharingjob_v01.ws.IWsdl2CodeEvents;
+
+/**
+ * Created by jCaal on 02/04/2015.
+ */
+public class empleos_publicados extends FragmentGenerico implements IWsdl2CodeEvents {
+    @Override
+    public void otrosParametros(String[] parms) {
+
+    }
+
+    @Override
+    public void hacerOnCreate() {
+
+    }
+
+    @Override
+    public void Wsdl2CodeStartedRequest() {
+
+    }
+
+    @Override
+    public void Wsdl2CodeFinished(String methodName, Object Data) {
+
+    }
+
+    @Override
+    public void Wsdl2CodeFinishedWithException(Exception ex) {
+
+    }
+
+    @Override
+    public void Wsdl2CodeEndedRequest() {
+
+    }
+}
